fb623353c787047aba8db5a0d952990751c0b79c
carlos cezar <carlosjsciaa@gmail.com>
2023-05-09 00:59:36 -0300

    pilha 1

--- unidade_2/pilha.c
+++ unidade_2/pilha.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define tam 3
+struct tipo_pilha
+{
+	int dados[tam];
+	int ini;
+	int topo;
+};
+
+
 
 int main(){
-	int a = 3;
-	int b = 4;
-	int soma = a + b;
-	printf("A soma entre os números é: %d", soma);
+	struct tipo_pilha pilha;
+	int elemento;
+	void push(int elemento);
+	{
+		if(pilha.topo == tam){
+			printf("fila cheia. \n");
+			system("pause");
+		}
+		else{
+			pilha.dados[pilha.topo] = elemento;
+			pilha.topo++;
+			printf("tudo ok");
+		}
+	}
+
+
 	return 0;
 }
