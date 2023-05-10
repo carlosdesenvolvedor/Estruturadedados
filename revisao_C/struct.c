0917052ccc5f48827286712241a74858a0993c3d
carlos <carlosjs_cia@hotmail.com>
2023-05-10 20:46:12 -0300

    relembrando string

--- revisao_C/struct.c
+++ revisao_C/struct.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 //pode se colocar varios tipos dentro da minha estrutura.
 typedef struct{
@@ -11,7 +12,7 @@ typedef struct{
 
 int main(){
 
-Aluno aluno;
+Aluno aluno, aluno2;
     aluno.nome[0] = 'C';
     aluno.nome[1] = 'a';
     aluno.nome[2] = 'r';
@@ -20,13 +21,22 @@ Aluno aluno;
     aluno.nome[5] = 's';
 
     aluno.idade =10;
+    printf("Primeiro aluno\n");
     aluno.sexo = 'M';
     printf("nome: ");
     for(int i = 0; i<6;i++){
         printf("%c",aluno.nome[i]);
     }
-    printf("\nidade: %d",aluno.idade);
+
+
+    aluno2.idade =45;
+    aluno2.sexo = 'F';
+    strcpy(aluno2.nome,"Maria josefina de oliveira");
+    
+    printf("\nidade: %d anos",aluno.idade);
     printf("\nSexo: %c\n",aluno.sexo);
+    printf("segundo aluno:\n");
+    printf("nome: %s\nSexo: %c\nIdade:%d anos",aluno2.nome,aluno2.sexo,aluno2.idade);
 
 
     
