1c15e48bb26b217d914ca1f628a0cbfc45be823a
carlos <carlosjs_cia@hotmail.com>
2023-05-10 21:57:15 -0300

    Erro no programa

--- revisao_C/string.c
+++ revisao_C/string.c
@@ -4,13 +4,15 @@
 
 
 int main(){ 
-    char  palavras[50]= {"Eu estou treinando a programação em C"};
+    char palavras[50]= {"Eu estou treinando a programação em C"};
+    char nome2[40];
     printf("Digite seu nome:");
     fgets(palavras,50,stdin);
     //A ultima posição é usado para o caracter de final da string \0
-    printf("nome: %s",palavras);
-    
-    
-    return 0;
+    printf("nome com fgets: %s\n",palavras);
+    printf("Digite outro nome: ");
+    scanf("%20[^\n]",palavras[50]);
+    printf("nome com scanf: %s",nome2);   
+    system("pause");
 
 }
