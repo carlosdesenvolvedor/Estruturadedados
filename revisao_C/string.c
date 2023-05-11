6507f62c10d8d4fa87a11b9a872b97a8e01c559e
carlos <carlosjs_cia@hotmail.com>
2023-05-10 21:06:23 -0300

    aprendendo a programar com a linguagem c

--- revisao_C/string.c
+++ revisao_C/string.c
@@ -4,9 +4,9 @@
 
 
 int main(){ 
-    char  palavras[5];
+    char  palavras[50]= {"Eu estou treinando a programação em C"};
     printf("Digite seu nome:");
-    fgets(palavras,5,stdin);
+    fgets(palavras,50,stdin);
     //A ultima posição é usado para o caracter de final da string \0
     printf("nome: %s",palavras);
     
