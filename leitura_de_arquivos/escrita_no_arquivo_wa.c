10e6f32ca51591c5522ad8a7435b9182b2c71e77
carlos <carlosjs_cia@hotmail.com>
2023-05-19 21:34:03 -0300

    ler e escrever em arquivos

---
+++ leitura_de_arquivos/escrita_no_arquivo_wa.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    FILE *arq;
+    char fruta[10];
+    
+    arq = fopen("frutas2.txt","a");//w para reescrever e a para sobscrever
+    if(arq){
+        //consegui criar o arquivo..
+        printf("informe uma fruta ou 0 para sair: ");
+        fgets(fruta,10,stdin); 
+        while(fruta[0] != '0'){
+            fputs(fruta,arq);
+            printf("informe uma fruta ou 0 para sair:");
+            fgets(fruta,10,stdin);
+        }
+    }
+    else{
+        printf("Nao foi criar o arquivo");
+    }
+    
+    fclose(arq);
+    return 0;
+}
