3ad8cff5c39d4abdd44a382672adeb216a1fa055
carlos <carlosjs_cia@hotmail.com>
2023-05-24 22:26:03 -0300

    testge

--- teste copy.c
+++ teste copy.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 //MEU NÓ
 typedef struct ELEMENTO{
         int data;
@@ -14,23 +14,55 @@ typedef struct ESTRUTURA{
 
 //NOVO DA VARIAVEL DA LISTA
 ESTRUTURA *e;
+typedef ELEMENTO *ptr_no;
+ptr_no lista;
 
-//para enserir na pilha.
+
+//prototipos
+void menu_mostrar();
+void menu_selecionar(int op);
+void insere(ELEMENTO *head);
+int remover(ELEMENTO* head);
+void pilha_mostrar(ELEMENTO* head);
+
+
+
+
+int main(){
+    srand(time(NULL));
+    int op = 1;
+    //Criando o primeiro nó da pilha
+    ELEMENTO* lista = (ELEMENTO *) malloc(sizeof(ELEMENTO));
+    lista->data = 0;
+    lista->next = NULL;
+    //Laço principal
+    while (op !=0){
+    system("cls");
+    menu_mostrar();
+    scanf("%d", &op);
+    menu_selecionar(op);
+    }
+    system("Pause");
+    return(0);
+    
+
+    return 0;
+}
 void menu_mostrar(){
- pilha_mostrar(e);
+ pilha_mostrar(lista);
  printf("\n\nEscolha uma das opcoes:\n");
  printf("1 - Inserir no final da pilha\n"); 
  printf("2 - Remover no final da pilha\n");
  printf("0 - Sair\n\n");
 }
-int op;
+
 void menu_selecionar(int op){
  switch (op){
     case 1:
-        insere(e);
+        insere(lista);
         break;
     case 2:
-        remover(e);
+        remover(lista);
         break;
  }
 }
@@ -63,27 +95,4 @@ void pilha_mostrar(ELEMENTO* head){
  head = head->next;
  }
  printf("%d, ", head->data);
-}
-
-
-
-int main(){
-    srand(time(NULL));
-    op = 1;
-    //Criando o primeiro nó da pilha
-    ELEMENTO* lista = (ELEMENTO *) malloc(sizeof(ELEMENTO));
-    lista->data = 0;
-    lista->next = NULL;
-    //Laço principal
-    while (op !=0){
-    system("cls");
-    menu_mostrar();
-    scanf("%d", &op);
-    menu_selecionar(op);
-    }
-    system("Pause");
-    return(0);
-    
-
-    return 0;
 }
