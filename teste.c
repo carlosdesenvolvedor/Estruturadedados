8b6e30c9530b3214db79c7e4849db6ec048cd0bd
carlos <carlosjs_cia@hotmail.com>
2023-05-25 18:47:33 -0300

    chato ccccccccccc

--- teste.c
+++ teste.c
@@ -1,96 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef struct ELEMENTO{
-        int data;
-        struct ELEMENTO *prox;
-}ELEMENTO;
- 
-typedef struct ESTRUTURA{
-        ELEMENTO* head;
-}ESTRUTURA;
- 
-ESTRUTURA *e;
- 
-void insere(){
-        ELEMENTO* p = (ELEMENTO *) malloc(sizeof(ELEMENTO));
-        p->data = rand() % 100;
-        p->next = e->head;
-        e->head = p;
+#include <time.h>
+//Definindo a estrutura da fila
+ typedef struct no{
+ int dado;
+ struct no *proximo;
+}no;
+//Definindo variáveis
+typedef no *ptr_no;
+ptr_no pilha;
+int op;
+//Prototipação
+void menu_mostrar();
+void menu_selecionar(int op);
+void pilha_inserir(ptr_no pilha);
+void pilha_remover(ptr_no pilha);
+void pilha_mostrar(ptr_no pilha);
+//Função Principal
+int main() {
+ //Inicializando máquina de números randômicos
+ srand(time(NULL));
+ op = 1;
+ //Criando o primeiro nó da pilha
+ pilha = (ptr_no) malloc(sizeof(no));
+ pilha->dado = 0;
+ pilha->proximo = NULL;
+ //Laço principal
+ while (op !=0){
+ system("cls");
+ menu_mostrar();
+ scanf("%d", &op);
+ menu_selecionar(op);
+ }
+ system("Pause");
+ return(0);
 }
- 
-int remove(){
-        ELEMENTO* p = e->head;
-        int data;
-        if(p != NULL){
-                        e->head = p->next;
-                        p->next = NULL;
-                        data = p->data;
-                        return data;
-        }else{
-                return -1;
-        }
+//Mostra o menu de opções
+void menu_mostrar(){
+ pilha_mostrar(pilha);
+ printf("\n\nEscolha uma das opcoes:\n");
+ printf("1 - Inserir no final da pilha\n"); 
+ printf("2 - Remover no final da pilha\n");
+ printf("0 - Sair\n\n");
 }
-void imprimir(){
-    ELEMENTO* ptr = e->head ;
-    printf("\ninicio ->");
-    while(ptr != NULL){
-        printf("%d ",ptr->data);
-        ptr = ptr->next;
-    }
-    printf("<- Fim\n\n");
+//Executa a opção escolhida no menu
+void menu_selecionar(int op){
+ switch (op){
+ case 1:
+ pilha_inserir(pilha);
+ break;
+ case 2:
+ pilha_remover(pilha);
+ break;
+ }
 }
-    void menu(){
-    printf("1 - Para inserir na pilha\n");
-    printf("2 - Para remover da pilha\n");
-    printf("3 - para imprimir \n");
-    printf("0 - para encerrar\n");
-
-    }
-int main(){
-        
-    int op,dado;
-    e->head = NULL;
-    
-    do{
-        system("cls");
-        menu();
-        scanf("%d",&op);
-        fflush(stdin);
-        system("cls");
-        switch (op)
-        {
-        case 1:
-            printf("\nDigite o dado para ser empilhado: ");
-            scanf("%d",&dado);
-            fflush(stdin);
-            insere(dado);
-            system("pause");
-            break;
-        case 2:
-        printf("\nremovendo da pilha: ");
-            dado = remove(); 
-            if(dado == -1){
-                printf("Erro\n");
-                system("pause");
-            }else{
-                printf("Removendo o elemento %d do inicio da lista.\n",dado);
-                system("pause");
-            }
-            break;
-        case 3:
-            printf("dados coletados");
-            imprimir();
-            system("pause");
-            break;    
-        default:
-            break;
-        }
-
-    }while(op != 0); 
-    
-    
-
-
-        return 0;
+//Insere um elemento no final da Pilha
+void pilha_inserir(ptr_no pilha){
+ while(pilha->proximo != NULL){
+ pilha = pilha->proximo;
+ }
+ pilha->proximo = (ptr_no) malloc(sizeof(no));
+ pilha = pilha->proximo;
+ pilha->dado = rand()%100;
+ pilha->proximo = NULL;
+}
+//Remove um elemento da pilha
+void pilha_remover(ptr_no pilha){
+ ptr_no atual;
+ atual = (ptr_no) malloc(sizeof(no));
+ while(pilha->proximo != NULL){
+ atual = pilha;
+ pilha = pilha->proximo;
+ }
+ atual->proximo = NULL;
+}
+//Desenha o conteúdo da pilha na tela
+void pilha_mostrar(ptr_no pilha){
+ system("cls");
+ while(pilha->proximo != NULL) {
+ printf("%d, ", pilha->dado);
+ pilha = pilha->proximo;
+ }
+ printf("%d, ", pilha->dado);
 }
